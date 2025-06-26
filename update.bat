5680bcb08282ee96acc7c44d8c99e9270362c6a1
LUCAS FRAZÃO <121047511+lucasfrazz@users.noreply.github.com>
2025-06-26 11:33:21 -0300

    atualizacao automatica: %date% %time%

--- update.bat
+++ update.bat
@@ -1,1 +1,16 @@
- 
+@echo off
+REM chcp 65001 > nul
+echo ========================================
+echo    ATUALIZADOR DO APP DE EMPREGO
+echo ========================================
+echo.
+echo Atualizando o repositorio...
+git add .
+git commit -m "atualizacao automatica: %date% %time%"
+git pull origin main --allow-unrelated-histories
+git push -u origin master
+echo.
+echo ========================================
+echo    ATUALIZACAO CONCLUIDA!
+echo ========================================
+echo.
